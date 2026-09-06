8b68c3487f1d40ebca213211ab4bb5587f053396
Johannes van Heerden <johannesvheerden@gmail.com>
2026-09-06 10:23:28 +0200

    Initial project init

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# sorting-vis
+# Sorting Algorithm Visualizer
+
+A visualization tool for common sorting algorithms built in C# and .NET using SFML.NET.
+
+## Technologies
+- C#
+- .NET
+- SFML.NET
---
+++ SortingVis/Program.cs
@@ -0,0 +1,13 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+
+var window = new RenderWindow(new VideoMode(new Vector2u(1280, 720)), "Sorting Visualization");
+window.Closed += (s, e) => window.Close();
+
+while (window.IsOpen)
+{
+    window.DispatchEvents();
+    window.Clear(Color.Green);
+    window.Display();
+}
